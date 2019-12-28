cd0287e104a491fd12c4db910c78fa4c402a7f0b
vidarmikaelsson <vidarmikaelsson@gmail.com>
2019-12-28 19:17:04 +0100

    Updates

---
+++ precache-manifest.3bab082627a79632e7d8db46f8164385.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc2120e76216f721b2218ec777b373e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "99aa8284f9335cdeacb2",
+    "url": "/monsters-rolodex/static/css/main.f517b602.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99aa8284f9335cdeacb2",
+    "url": "/monsters-rolodex/static/js/main.29dd3d02.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
